41563933c0e11c6f28cfc3e85524e3c2fcfcf62f
Admin <2216528769@qq.com>
2024-12-28 21:37:34 +0800

    Init

---
+++ 一键输入/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace 一键输入
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			resources.ApplyResources(this.label1, "label1");
+			this.label1.BackColor = System.Drawing.Color.MediumSpringGreen;
+			this.label1.Name = "label1";
+			this.label1.Click += new System.EventHandler(this.label1_Click);
+			// 
+			// richTextBox1
+			// 
+			resources.ApplyResources(this.richTextBox1, "richTextBox1");
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+			// 
+			// richTextBox2
+			// 
+			resources.ApplyResources(this.richTextBox2, "richTextBox2");
+			this.richTextBox2.Name = "richTextBox2";
+			this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+			// 
+			// label2
+			// 
+			resources.ApplyResources(this.label2, "label2");
+			this.label2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+			this.label2.Name = "label2";
+			this.label2.Click += new System.EventHandler(this.label2_Click);
+			// 
+			// label3
+			// 
+			resources.ApplyResources(this.label3, "label3");
+			this.label3.ForeColor = System.Drawing.Color.IndianRed;
+			this.label3.Name = "label3";
+			// 
+			// Form1
+			// 
+			resources.ApplyResources(this, "$this");
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Control;
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.richTextBox2);
+			this.Controls.Add(this.richTextBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.RichTextBox richTextBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+	}
+}
+
---
+++ 一键输入/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using WindowsInput;
+using WindowsInput.Native;
+
+
+namespace 一键输入
+{
+	public partial class Form1 : Form
+	{
+
+		// 引入 Windows API
+		[DllImport("user32.dll", CharSet = CharSet.Auto)]
+		public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+		[DllImport("user32.dll")]
+		public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+		// 定义快捷键
+		private const int HOTKEY_ID = 1;
+		private const uint MOD_CONTROL = 0x0002; // Ctrl 键
+		private const uint VK_V = 0x56; // V 键（Ctrl+V）
+		public Form1()
+		{
+			InitializeComponent();
+
+			// 注册 Ctrl+V 快捷键
+			if (RegisterHotKey(this.Handle, HOTKEY_ID, MOD_CONTROL, VK_V))
+			{
+				MessageBox.Show("快捷键已注册！");
+			}
+			else
+			{
+				MessageBox.Show("快捷键注册失败！");
+			}
+		}
+		protected override void WndProc(ref Message m)
+		{
+			// 检查是否为我们注册的快捷键
+			if (m.Msg == 0x0312) // WM_HOTKEY
+			{
+				// 快捷键被按下时调用模拟输入的方法
+				string clipboardText = Clipboard.GetText(); // 获取剪切板的内容
+				if (!string.IsNullOrEmpty(clipboardText))
+				{
+					SimulateTextInput(clipboardText); // 模拟输入剪切板的文本
+				}
+			}
+
+			base.WndProc(ref m);
+		}
+		public void SimulateTextInput(string text)
+		{
+			InputSimulator simulator = new InputSimulator();
+
+			// 遍历文本中的每个字符
+			foreach (var c in text)
+			{
+				if (c == '\n' || c == '\r')
+				{
+					// 如果是换行符，模拟回车
+					simulator.Keyboard.KeyPress(VirtualKeyCode.RETURN);
+				}
+				else
+				{
+					// 输入文本中的每个字符
+					simulator.Keyboard.TextEntry(c);
+				}
+				Thread.Sleep(10); // 模拟自然的打字速度（可调节）
+			}
+		}
+		protected override void OnFormClosed(FormClosedEventArgs e)
+		{
+			UnregisterHotKey(this.Handle, HOTKEY_ID); // 注销快捷键
+			base.OnFormClosed(e);
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void tabPage1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void textBox1_TextChanged_1(object sender, EventArgs e)
+		{
+
+		}
+
+		private void richTextBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void richTextBox2_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label3_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
